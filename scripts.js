c617c82a4110bee429ec34b28d3185b8b90768b0
lfernandogoulart <1997goulart@gmail.com>
2025-10-21 19:01:33 -0300

    primeiro commint - projeto starbacks

---
+++ scripts.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+let imagem = document.querySelector(".Starbucks")
+let fundo = document.querySelector(".circulo")
+
+
+
+function trocaImagem(endereco) {
+    imagem.src = endereco
+
+}
+
+function trocaFundo(cor) {
+    fundo.style.background = cor
+
+}
+
+
+
+
+
